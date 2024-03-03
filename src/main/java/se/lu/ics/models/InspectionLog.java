faf5a27b252818a8b141e3181aeef8906ed01290
Emelie <em1273vo-s@student.lu.se>
2024-03-03 16:24:31 +0100

    Implemented CRUD-functions for Warehouse

---
+++ src/main/java/se/lu/ics/models/InspectionLog.java
@@ -0,0 +1,74 @@
+package se.lu.ics.models;
+
+import java.time.LocalDate;
+
+public class InspectionLog {
+
+    private Shipment shipment;
+    private Warehouse warehouse;
+    private LocalDate date;
+    private String inspector;
+    private String result;
+
+    public InspectionLog() {
+    }
+
+    public InspectionLog(Shipment shipment, Warehouse warehouse, LocalDate date, String inspector, String result) {
+        this.shipment = shipment;
+        this.warehouse = warehouse;
+        this.date = date;
+        this.inspector = inspector;
+        this.result = result;
+        shipment.addInspection(this);
+        warehouse.addInspection(this);
+
+    }
+
+    public Shipment getShipment() {
+        return shipment;
+    }
+
+    public void setShipment(Shipment shipment) {
+        this.shipment = shipment;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getInspector() {
+        return inspector;
+    }
+
+    public void setInspector(String inspector) {
+        this.inspector = inspector;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public static String inspectionHistory(Shipment shipment){
+        return "Inspection history for shipment with ID: " + shipment.getShipmentId() + shipment.getInspectionsMade();
+    }
+
+    @Override
+    public String toString() {
+        return  "\n\nShipmentID=" + shipment.getShipmentId() +
+                "\nWarehouse=" + warehouse.getName() +
+                "\nDate='" + date +
+                "\nInspector='" + inspector +
+                "\nResult='" + result +
+                "\n";
+    }
+
+
+}
